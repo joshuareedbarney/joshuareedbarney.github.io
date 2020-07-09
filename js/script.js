9d37138e9697a6c19ac3cd247c1394c7a3c34532
Joshua Reed Barney <joshuareedbarney@gmail.com>
2020-07-09 14:29:22 -0600

    Made it so that the array is shuffled instead of a random number generated, this makes you go through all of the quotes before a duplicate

--- js/script.js
+++ js/script.js
@@ -5,15 +5,33 @@ Joshua Barney
 July 9th 2020
 */
 
+var cur_index = 0;
+
+//https://www.w3schools.com/js/js_random.asp
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-var main = function() {
-    var quote = quotes[getRndInteger(0, quotes.length)];
+var newQuote = function() {
+    // getRndInteger(0, quotes.length)
+    var quote = quotes[cur_index];
+    cur_index = (cur_index + 1) % quotes.length;
+    console.log(cur_index);
     //console.log(quote);
     document.querySelector("#quote").innerHTML = quote.quote;
     document.querySelector("#author").innerHTML = quote.author;
 }
 
-// window.onload = main;
+//https://javascript.info/task/shuffle
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+var main = function() {
+    shuffle(quotes);
+}
+
+window.onload = main;
