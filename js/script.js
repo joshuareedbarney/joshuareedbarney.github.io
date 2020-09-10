e331709af056394df020373848ca9af9fabf2831
Joshua Reed Barney <joshuareedbarney@gmail.com>
2020-09-10 11:47:48 -0600

    Added functionality for the right and left arrow keys

--- js/script.js
+++ js/script.js
@@ -14,9 +14,20 @@ function getRndInteger(min, max) {
 
 var newQuote = function() {
     // getRndInteger(0, quotes.length)
-    var quote = quotes[cur_index];
     cur_index = (cur_index + 1) % quotes.length;
+    var quote = quotes[cur_index];
     //console.log(quote);
+    document.querySelector("#quote").innerHTML = quote.quote;
+    document.querySelector("#author").innerHTML = quote.author;
+}
+
+var lastQuote = function() {
+    cur_index = cur_index - 1;
+    if (cur_index < 0) {
+        cur_index = quotes.length - 1;
+    }
+
+    var quote = quotes[cur_index];
     document.querySelector("#quote").innerHTML = quote.quote;
     document.querySelector("#author").innerHTML = quote.author;
 }
@@ -31,6 +42,20 @@ function shuffle(array) {
 
 var main = function() {
     shuffle(quotes);
+
+    //https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript
+    document.querySelector("html").addEventListener('keydown', function(event) {
+        const key = event.key;
+
+        switch (key) {
+            case "ArrowRight":
+                newQuote();
+                break;
+            case "ArrowLeft":
+                lastQuote();
+                break;
+        }
+    });
 }
 
 // window.onload = main;
